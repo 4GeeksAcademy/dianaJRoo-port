8d21446f3c9bf0cd0974c4b63fcc2f37a74f70ad
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-05-21 06:38:43 +0000

    avance

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,30 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		<>
+      
+      <div id='hero-body' className="flex justify-center items-center mt-0 mb-0 bg-gradient-to-r from-stone-400 via-stone-200 to-stone-300 h-screen">
+            
+            <div id='hero' className='d-flex flex-column'  >
+                <h1 className='text-5xl text-gray-500 font-bold text-center'>
+            
+                </h1>
+                
+            </div>
+        </div>
+       
+        <div className='border-y-4 flex justify-center items-center py-4 gap-6 shadow'>
+                <div onClick={() => actions.logOut()} className='flex items-center gap-2 cursor-pointer'>
+                    <img src='https://numero.twic.pics/2024-03/taylor-swift-album-numero-magazine1.jpg?twic=v1/quality=83/truecolor=true/output=jpeg' className='flag h-10 w-10 bg-orange-300 rounded-full top-1 start-1' />
+                    <p className="font-bold text-xl">Salir</p>
+                </div>
+             
+            </div >
+
+
+
+
+    
+    </>
 	);
 };
